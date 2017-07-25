6a0f892c29e321d292afbd571fe7328c2c3e1d32
jsergio123 <jsergio123@gmail.com>
2017-07-24 21:19:50 -0400

    Update resolver.py

--- lib/urlresolver/resolver.py
+++ lib/urlresolver/resolver.py
@@ -142,7 +142,7 @@ class UrlResolver(object):
             '<setting id="%s_enabled" ''type="bool" label="%s" default="true"/>' % (cls.__name__, common.i18n('enabled'))
         ]
         if include_login:
-            xml.append('<setting id="%s_login" ''type="bool" label="%s" default="false" visible="false"/>' % (cls.__name__, common.i18n('login')))
+            xml.append('<setting id="%s_login" ''type="bool" label="%s" default="false" visible="false"/>' % (cls.__name__, common.i18n('Login')))
         return xml
 
     @classmethod
